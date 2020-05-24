641772afc0b1af677b5e8c7cfecad8563e429a6a
Datt <datt1994@gmail.com>
2020-05-25 03:44:27 +0530

    Add Carthage support

---
+++ DPOTPView/DPOTPView/DPOTPView.h
@@ -0,0 +1,19 @@
+//
+//  DPOTPView.h
+//  DPOTPView
+//
+//  Created by Datt Patel on 25/05/20.
+//  Copyright © 2020 datt. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DPOTPView.
+FOUNDATION_EXPORT double DPOTPViewVersionNumber;
+
+//! Project version string for DPOTPView.
+FOUNDATION_EXPORT const unsigned char DPOTPViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DPOTPView/PublicHeader.h>
+
+
